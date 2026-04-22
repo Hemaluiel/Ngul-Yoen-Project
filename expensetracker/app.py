fc4bc0e84ef9f47e5e035f6256cbf350d4e7cb5f
Hemaluiel <03230070.gcbs@rub.edu.bt>
2026-04-22 11:02:50 +0600

    modified

--- expensetracker/app.py
+++ expensetracker/app.py
@@ -112,6 +112,9 @@ def extract_top_expenses(rows):
 
 
 
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=10000)
 
