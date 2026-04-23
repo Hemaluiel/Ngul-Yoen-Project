c14cefd7cfc353624e05ef95d9ace0dd87429fc6
Hemaluiel <03230070.gcbs@rub.edu.bt>
2026-04-23 21:01:10 +0600

    modified

--- backend/utils/pdf_reader.py
+++ backend/utils/pdf_reader.py
@@ -5,6 +5,8 @@ def extract_text(pdf_file):
 
     with pdfplumber.open(pdf_file) as pdf:
         for page in pdf.pages:
-            text += page.extract_text() + "\n"
+            page_text = page.extract_text()
+            if page_text:
+                text += page_text + "\n"
 
     return text
